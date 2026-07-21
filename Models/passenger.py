08b595db7ae419a5f7378ca9c8027bf53e84dbc4
Uttam Mishra <ayushmishra47893@gmail.com>
2026-07-21 07:20:14 +0530

    feat(models): add passenger model

---
+++ Models/passenger.py
@@ -0,0 +1,18 @@
+class Passenger:
+    def __init__(self, name, email, phone, password, date_joined):
+        self.name = name
+        self.email =  email
+        self.phone = phone
+        self.password = password
+        self.date_joined = date_joined
+        # self. = 
+        # self. = 
+        # self. = 
+
+
+    def display(self):
+        print(f"Name : {self.name}")
+        print(f"Email : {self.email}")
+        print(int(f"Phone : {self.phone}"))
+        print(f"Password : {self.password}")
+        print(f"Date Joined : {self.date_joined}")
