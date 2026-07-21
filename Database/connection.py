5c57e3d42af797490e895e6106bd937dca8acc7c
Uttam Mishra <ayushmishra47893@gmail.com>
2026-07-22 04:35:18 +0530

    refactor : remove some bugs

--- Database/connection.py
+++ Database/connection.py
@@ -2,14 +2,13 @@ import mysql.connector
 
 
 def get_connection():
-    return mysql.connector.connect(
+    connection = mysql.connector.connect(
         host="localhost",
         user="root",
         password="5448",
         database="airline_db"
     )
 
-print("Connected successfully!")
-
+    return connection
 
 
