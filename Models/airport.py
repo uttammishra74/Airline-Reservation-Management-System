61ad5180c825e730d6359180358a5fad10a7edf8
Uttam Mishra <ayushmishra47893@gmail.com>
2026-07-24 04:14:12 +0530

    fix: prevent duplicate function

--- Models/airport.py
+++ Models/airport.py
@@ -1,14 +1,14 @@
-class Airport:
-    def __init__(self, airport_id, airport_name, airport_code, city, country):
-        self.airport_id = airport_id
-        self.airport_name = airport_name
-        self.airport_code = airport_code
-        self.city = city
-        self.country = country
+# class Airport:
+#     def __init__(self, airport_id, airport_name, airport_code, city, country):
+#         self.airport_id = airport_id
+#         self.airport_name = airport_name
+#         self.airport_code = airport_code
+#         self.city = city
+#         self.country = country
 
-    def display(self):
-        print(f"Airport ID   : {self.airport_id}")
-        print(f"Airport Name : {self.airport_name}")
-        print(f"Airport Code : {self.airport_code}")
-        print(f"City         : {self.city}")
-        print(f"Country      : {self.country}")
+#     def display(self):
+#         print(f"Airport ID   : {self.airport_id}")
+#         print(f"Airport Name : {self.airport_name}")
+#         print(f"Airport Code : {self.airport_code}")
+#         print(f"City         : {self.city}")
+#         print(f"Country      : {self.country}")
