01bed2c4538f27c9ea96aaf599aec99e31143daf
Uttam Mishra <ayushmishra47893@gmail.com>
2026-07-25 05:44:05 +0530

    fix : update these models accord to new function

--- Models/passenger.py
+++ Models/passenger.py
@@ -1,15 +1,15 @@
-class Passenger:
-    def __init__(self, name, email, phone, password, date_joined):
-        self.name = name
-        self.email =  email
-        self.phone = phone
-        self.password = password
-        self.date_joined = date_joined
+# class Passenger:
+#     def __init__(self, name, email, phone, password, date_joined):
+#         self.name = name
+#         self.email =  email
+#         self.phone = phone
+#         self.password = password
+#         self.date_joined = date_joined
 
 
-    def display(self):
-        print(f"Name : {self.name}")
-        print(f"Email : {self.email}")
-        print(int(f"Phone : {self.phone}"))
-        print(f"Password : {self.password}")
-        print(f"Date Joined : {self.date_joined}")
+#     def display(self):
+#         print(f"Name : {self.name}")
+#         print(f"Email : {self.email}")
+#         print(int(f"Phone : {self.phone}"))
+#         print(f"Password : {self.password}")
+#         print(f"Date Joined : {self.date_joined}")
